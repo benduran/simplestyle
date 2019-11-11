1088db13f06491f3c6c8331b4810b65124c51907
Benjamin Duran <bduran@netflix.com>
2019-11-11 07:48:54 -0800

    updated exports
    - moved typings back to the original commonjs export

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export { getAll as getAllSheets } from './sheetCache';
 export { default as rawStyles } from './rawStyles';
 export { setSeed } from './clasnameSeed';
 export { default as keyframes } from './keyframes';
+export { IIndexableCSSProperties, IRawStyles, ISimpleStyleRules, ISimpleStyleSheet } from './styleTypes';
 
 export default createStyles.default;
