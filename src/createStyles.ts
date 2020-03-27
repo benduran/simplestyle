d7b3a55c510088fb0dd94243ca1fecb4fa386f59
Benjamin Duran <stratodyne@gmail.com>
2020-03-26 20:11:20 -0700

    updated core logic to be simpler but with the same features

--- src/createStyles.ts
+++ src/createStyles.ts
@@ -1,3 +1,6 @@
+
+import { Properties } from 'csstype';
+
 import { SimpleStyleRules } from './types';
 import generateClassName from './generateClassName';
 
@@ -14,38 +17,50 @@ function formatCSSRuleName(rule: string): string {
   return rule.replace(/([A-Z])/g, p1 => `-${p1.toLowerCase()}`);
 }
 
+function formatCSSRules(cssRules: Properties): string {
+  return Object.entries(cssRules).reduce((prev, [cssProp, cssVal]) => `${formatCSSRuleName(cssProp)}:${cssVal};`, '');
+}
+
 type ParentSelector = string | null;
 
 function execCreateStyles<
   T extends SimpleStyleRules,
   K extends keyof T,
-  O extends { [classname in K]: string },
+  O extends { [classKey in K]: string },
 >(
   rules: T,
   options: CreateStylesOptions,
   parentSelector: ParentSelector,
-): O {
+): [O, string] {
+  let sheetContents = '';
   const out = {} as O;
   const styleEntries = Object.entries(rules);
   for (const [classNameOrCSSRule, classNameRules] of styleEntries) {
-    let generated = '';
-    if (isNestedSelector(classNameOrCSSRule)) {
-      if (!parentSelector) throw new Error('Unable to generate nested selector because parentSelector is null');
-      generated = classNameOrCSSRule.replace(/&/g, parentSelector);
-    } else generated = generateClassName(classNameOrCSSRule);
-    if (typeof classNameRules === 'object') execCreateStyles(classNameRules as SimpleStyleRules, options, generated);
+    // if the classNameRules is a string, we are dealing with a display: none; type rule
+    if (typeof classNameRules === 'string') {
+      sheetContents += `${formatCSSRuleName(classNameOrCSSRule)}:${classNameRules};`;
+    } else if (isNestedSelector(classNameOrCSSRule)) {
+      if (!parentSelector) throw new Error('Unablet to generate nested rule because parentSelector is missing');
+      // format of { '& > span': { display: 'none' } } (or further nesting)
+      const replaced = classNameOrCSSRule.replace(/&/g, parentSelector);
+      sheetContents += `${replaced}{${execCreateStyles(classNameRules as T, options, replaced)[1]}}`;
+    } else if (!parentSelector && typeof classNameRules === 'object') {
+      const generated = generateClassName(classNameOrCSSRule);
+      (out as any)[classNameOrCSSRule] = generated;
+      sheetContents += `.${generated}{${execCreateStyles(classNameRules as T, options, generated)[1]}}`;
+    }
   }
-  return out;
+  return [out, sheetContents];
 }
 
 export default function createStyles<
   T extends SimpleStyleRules,
   K extends keyof T,
-  O extends { [classname in K]: string },
+  O extends { [classKey in K]: string },
 >(
   rules: T,
   options?: Partial<CreateStylesOptions>,
-): O {
+): [O, string] {
   const coerced: CreateStylesOptions = {
     accumulate: options?.accumulate || false,
     flush: options?.flush || true,
--- test/createStyles.spec.ts
+++ test/createStyles.spec.ts
@@ -4,17 +4,22 @@ import { SimpleStyleRules } from '../src/types';
 
 describe('createStyles tests', () => {
   it('Should generate some basic styles', () => {
-    const styles: SimpleStyleRules = {
+    const rules: SimpleStyleRules = {
       one: {
         display: 'flex',
       },
       two: {
-        '& > div': {
-          color: 'pink',
-        },
         backgroundColor: 'red',
       },
     };
-    createStyles(styles);
+    const [styles, styleContents] = createStyles(rules);
+
+    Object.keys(rules).forEach((key) => {
+      expect(styles[key]).toBeDefined();
+      expect(styles[key].length).toBeGreaterThan(0);
+      expect(styles[key]).toContain(key);
+    });
+    expect(styleContents).toContain(`.${styles.one}{display:flex;}`);
+    expect(styleContents).toContain(`.${styles.two}{background-color:red;}`);
   });
 });
