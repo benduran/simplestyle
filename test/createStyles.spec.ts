a9202852b495e3f82fb92cc654921cf44f9aedbb
Benjamin Duran <stratodyne@gmail.com>
2020-03-27 17:22:56 -0700

    fixed broken tests

--- test/createStyles.spec.ts
+++ test/createStyles.spec.ts
@@ -23,7 +23,7 @@ describe('createStyles tests', () => {
     expect(styleContents).toContain(`.${styles.one}{display:flex;position:fixed;}`);
     expect(styleContents).toContain(`.${styles.two}{background-color:red;}`);
   });
-  it('Should generate some basic styles for a simple nested structure', () => {
+  it.only('Should generate some basic styles for a simple nested structure', () => {
     const rules: SimpleStyleRules = {
       nested: {
         '& > span': {
@@ -50,7 +50,8 @@ describe('createStyles tests', () => {
     expect(styles.yarg.length).toBeGreaterThan(0);
     expect(styles.yarg).toContain('yarg');
 
-    expect(styleContents).toContain(`.${styles.nested} > span{font-family:Arial;}.${styles.nested}{font-size:20px;}`);
+    expect(styleContents).toContain(`.${styles.nested} > span{font-family:Arial;}`);
+    expect(styleContents).toContain(`.${styles.nested}{font-size:20px;}`);
     expect(styleContents).toContain(`.${styles.yarg}:hover{top:-1px;}`);
     expect(styleContents).toContain(`.${styles.yarg}:focus{background-color:purple;}`);
   });
