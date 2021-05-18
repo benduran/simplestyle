5081e7257548065450ad266d86a42b7584fe6550
benduran <stratodyne@gmail.com>
2021-05-17 20:07:34 -0700

    fix: fixed the prepbuild script

--- prepBuild.js
+++ prepBuild.js
@@ -14,34 +14,24 @@ execSync('tsc --project ./tsconfig.json', { stdio: 'inherit' });
 
 execSync('tsc --project ./tsconfig.commonjs.json', { stdio: 'inherit' });
 
-execSync('cp ./.npmignore ./dist', { stdio: 'inherit' });
+// execSync('cp ./.npmignore ./dist', { stdio: 'inherit' });
 
-setTimeout(() => {
-  // We prepare the source maps so they're not wrong
-  allFiles
-    .filter(fp => fp.endsWith('.map'))
-    .forEach(fp => {
-      const contents = JSON.parse(fs.readFileSync(fp, 'utf8'));
-      for (let i = 0; i < contents.sources.length; i++) {
-        contents.sources[i] = `./${path.basename(contents.sources[i])}`;
-        fs.writeFileSync(fp, JSON.stringify(contents), 'utf8');
-      }
-    });
+// We prepare the source maps so they're not wrong
+allFiles
+  .filter(fp => fp.endsWith('.map'))
+  .forEach(fp => {
+    const contents = JSON.parse(fs.readFileSync(fp, 'utf8'));
+    for (let i = 0; i < contents.sources.length; i++) {
+      contents.sources[i] = `./${path.basename(contents.sources[i])}`;
+      fs.writeFileSync(fp, JSON.stringify(contents), 'utf8');
+    }
+  });
 
-  // Copy the package.json file from the root and prep it so that we don't get the cruft
-  // from the root
-  const pJsonContents = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
-  delete pJsonContents.devDependencies;
-  delete pJsonContents.scripts;
+// Copy the package.json file from the root and prep it so that we don't get the cruft
+// from the root
+const pJsonContents = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
+delete pJsonContents.devDependencies;
+delete pJsonContents.scripts;
 
-  // Set all files that should be included
-  pJsonContents.files = allFiles
-    .filter(fp => fp.endsWith('.js') || fp.endsWith('.map') || fp.endsWith('.ts'))
-    .map(fp => fp.replace(distDir, '.'));
-  fs.writeFileSync(path.join(distDir, 'package.json'), JSON.stringify(pJsonContents, null, 2), 'utf8');
-  setTimeout(() => {
-    process.chdir(distDir);
-
-    execSync('npm pack', { stdio: 'inherit' });
-  }, 1000);
-}, 1000);
+// Set all files that should be included
+fs.writeFileSync(path.join(distDir, 'package.json'), JSON.stringify(pJsonContents, null, 2), 'utf8');
