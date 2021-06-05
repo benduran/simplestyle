8de045979da09103fec1013d7d8360de3eee0b9d
benduran <stratodyne@gmail.com>
2021-06-04 21:23:37 -0700

    chore: added ts-jest and TSC build

--- src/generateClassName.ts
+++ src/generateClassName.ts
@@ -8,8 +8,10 @@ export function setSeed(seed: number | null): void {
     return;
   }
   if (typeof seed !== 'number') throw Error('Unable to setSeed as provided seed was not a valid number');
-  if (seed === Number.MAX_SAFE_INTEGER) throw Error('Unable to setSeed because the seed was already the maximum safe JavaScript number allowed');
-  if (seed === Number.POSITIVE_INFINITY || seed === Number.NEGATIVE_INFINITY) throw new Error('Unable to setSeed. Positive or negative infinity is not allowed');
+  if (seed === Number.MAX_SAFE_INTEGER)
+    throw Error('Unable to setSeed because the seed was already the maximum safe JavaScript number allowed');
+  if (seed === Number.POSITIVE_INFINITY || seed === Number.NEGATIVE_INFINITY)
+    throw new Error('Unable to setSeed. Positive or negative infinity is not allowed');
   if (seed < 0) throw new Error('Unable to setSeed. Seed must be a number >= 0');
   inc = seed;
 }
@@ -25,7 +27,7 @@ export function getUniqueSuffix(): string {
     result = numPairsRegex.exec(incStr);
   }
   let out = '_';
-  numPairs.forEach((pair) => {
+  numPairs.forEach(pair => {
     const val = +pair;
     if (val > 25) {
       const [first, second] = pair.split('');
