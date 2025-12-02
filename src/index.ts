491efd5fc19b9770ecbbb77e27fda30b650eb7e5
benduran <stratodyne@gmail.com>
2025-12-02 11:15:57 +0000

    fix: forgot to export the new function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export type { CreateStylesArgs, CreateStylesOptions } from './createStyles.js';
-export { default as createStyles, keyframes, rawStyles } from './createStyles.js';
+export { default as createStyles, keyframes, makeCreateStyle, rawStyles } from './createStyles.js';
 export { setSeed } from './generateClassName.js';
 export type { PosthookPlugin } from './plugins.js';
 export { registerPosthook } from './plugins.js';
