de0ef26889f301af2d65ebfe9b173adacba355af
benduran <stratodyne@gmail.com>
2025-12-06 19:29:42 +0000

    feat: added a next.js and astro wrapper for convenience

---
+++ src/next/SimpleStyleProvider.tsx
@@ -0,0 +1,28 @@
+import type { PropsWithChildren } from 'react';
+import type { SimpleStyleRegistry } from '../simpleStyleRegistry.js';
+
+export type SimpleStyleProviderProps = PropsWithChildren & {
+  registry: SimpleStyleRegistry;
+};
+
+/**
+ * Accumulates all CSS rules and writes
+ * them to your layout.
+ * Use this for Next.js or other Next.js-like frameworks
+ * that leverage React server components
+ */
+export function SimpleStyleProvider({
+  children,
+  registry,
+}: SimpleStyleProviderProps) {
+  return (
+    <>
+      {registry.getRulesById().map(([ruleId, css]) => (
+        <style id={ruleId} key={ruleId}>
+          {css}
+        </style>
+      ))}
+      {children}
+    </>
+  );
+}
