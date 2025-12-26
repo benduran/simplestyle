6c33a9b51eb34bdda5f56427d41f83f3ccf040a6
benduran <stratodyne@gmail.com>
2025-12-26 20:22:23 +0000

    Version Bump
    simplestyle-js@5.3.1-beta.1

--- src/astro/index.ts
+++ src/astro/index.ts
@@ -2,11 +2,12 @@ import { makeCssFuncs as _makeCssFuncs } from '../makeStyles.js';
 import { makeSimpleStyleProvider } from '../SimpleStyleProvider.js';
 import { SimpleStyleRegistry } from '../simpleStyleRegistry.js';
 
-export const makeCssFuncs: typeof _makeCssFuncs = (opts) => {
+export const makeCssFuncs = (...args: Parameters<typeof _makeCssFuncs>) => {
+  const [opts, ...rest] = args;
   const { registry: providedRegistry } = opts;
   const registry = providedRegistry ?? new SimpleStyleRegistry();
 
-  const funcs = _makeCssFuncs({ ...opts, registry });
+  const funcs = _makeCssFuncs({ ...opts, registry }, ...rest);
   return {
     ...funcs,
     SimpleStyleProvider: makeSimpleStyleProvider(registry),
