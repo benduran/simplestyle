c0923825b9bcb9bbbb08db3773398a40325e4c41
benduran <stratodyne@gmail.com>
2026-01-14 15:38:03 +0000

    chore: moved usestate usage to a client component

--- src/next/ClientBoundary.tsx
+++ src/next/ClientBoundary.tsx
@@ -1,16 +1,37 @@
 'use client';
 
+import { type PropsWithChildren, useEffect, useState } from 'react';
 // this client boundary is required to allow
 // Next.js to collect the styles on the server
 // but allow flushing them to the DOM at runtime
 
 import type { SimpleStyleRegistry } from '../simpleStyleRegistry.js';
+import type { Nullish } from '../types.js';
 
 export function ClientBoundary({
-  rules,
-}: {
-  rules: ReturnType<SimpleStyleRegistry['getRulesById']>;
+  children,
+  registry,
+}: PropsWithChildren & {
+  registry: SimpleStyleRegistry;
 }) {
+  /** state */
+  const [rules, setRules] =
+    useState<Nullish<ReturnType<typeof registry.getRulesById>>>(null);
+
+  /** effects */
+  useEffect(() => {
+    const handleSettled = () => {
+      setRules(registry.getRulesById());
+    };
+    registry.on('settled', handleSettled);
+
+    return () => {
+      registry.off('settled', handleSettled);
+    };
+  }, [registry]);
+
+  if (!rules) return null;
+
   return (
     <>
       {rules.map(([ruleId, css]) => (
@@ -18,6 +39,7 @@ export function ClientBoundary({
           {css}
         </style>
       ))}
+      {children}
     </>
   );
 }
--- src/next/SimpleStyleProvider.tsx
+++ src/next/SimpleStyleProvider.tsx
@@ -1,6 +1,5 @@
-import { type PropsWithChildren, useEffect, useState } from 'react';
+import type { PropsWithChildren } from 'react';
 import type { SimpleStyleRegistry } from '../simpleStyleRegistry.js';
-import type { Nullish } from '../types.js';
 import { ClientBoundary } from './ClientBoundary.js';
 /**
  * Accumulates all CSS rules and writes
@@ -12,28 +11,5 @@ export function SimpleStyleProvider({
   children,
   registry,
 }: PropsWithChildren & { registry: SimpleStyleRegistry }) {
-  /** state */
-  const [rules, setRules] =
-    useState<Nullish<ReturnType<typeof registry.getRulesById>>>(null);
-
-  /** effects */
-  useEffect(() => {
-    const handleSettled = () => {
-      setRules(registry.getRulesById());
-    };
-    registry.on('settled', handleSettled);
-
-    return () => {
-      registry.off('settled', handleSettled);
-    };
-  }, [registry]);
-
-  if (!rules) return null;
-
-  return (
-    <>
-      <ClientBoundary rules={rules} />
-      {children}
-    </>
-  );
+  return <ClientBoundary registry={registry}>{children}</ClientBoundary>;
 }
